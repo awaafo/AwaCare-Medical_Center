ad37737df51adac20f9143de18047efd01604a7a
awaafo <98856828+awaafo@users.noreply.github.com>
2024-10-01 20:24:00 -0400

    Update processorder.php

--- processorder.php
+++ processorder.php
@@ -1,1 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect and sanitize input data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $service = htmlspecialchars($_POST['service']);
+    $date = htmlspecialchars($_POST['date']);
+    $notes = htmlspecialchars($_POST['notes']);
+
+    // You can add code to save the appointment data to a database or send an email
+
+    echo "Thank you, $name. Your appointment for $service on $date has been submitted.";
+} else {
+    echo "Invalid request.";
+}
+?>
 
